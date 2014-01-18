755a5f207207400a0f85951ba16e6af6450d3429
Tim Poisot <timothee.poisot@uqar.ca>
2014-01-18 13:11:00 -0500

    First commit

---
+++ pymangal/api.py
@@ -0,0 +1,42 @@
+import json
+import requests as re
+
+class mangal:
+    """
+    This class will handle connection to the API.
+
+    Arguments
+    url : The URL of the api
+    usr : An optional username
+    pwd : An optional password
+
+    """
+
+    def __init__(self, url='http://mangal.uqar.ca', usr=None, pwd=None):
+        auth = None
+        # We check that the URL is a string
+        if not isinstance(url, str):
+            raise TypeError("The URL must be a string")
+        # Let's check a bunch of things about the username and password
+        if (usr == None) and (pwd != None):
+            raise ValueError("You must provide a username")
+        if (usr != None) and (pwd == None):
+            raise ValueError("You must provide a password")
+        # Finally if all is well, we check the type and create a auth object
+        if (usr != None) and (pwd != None):
+            if isinstance(usr, str) and isinstance(pwd, str):
+                auth = (usr, pwd)
+            else :
+                raise TypeError("Both the password and the username must be strings")
+        # We want the URL to end with a trailing slash
+        if not url[-1:] == '/':
+            url += '/'
+        # Now we create the URL property
+        self.url = url + 'api/v1/'
+        # We establish first contact with the API
+        # Simply enough, we try to reach the root, and check the response code
+        API = re.get(self.url, auth = auth)
+        if not API.status_code == 200 :
+            raise ValueError("The URL you provided ("+url+") gave a "+str(API.status_code)+" status code")
+        self.resources = API.json().keys()
+
---
+++ test_pymangal.py
@@ -0,0 +1,27 @@
+import pymangal
+import unittest
+
+from pymangal import api
+
+class api_test(unittest.TestCase):
+    def test_URL_is_a_string(self):
+        self.assertRaises(TypeError, lambda : api.mangal(url = 4))
+    def test_Fake_URLs_give_404(self):
+        self.assertRaises(ValueError, lambda : api.mangal(url = 'http://t.co/'))
+    def test_if_usr_then_pwd(self):
+        self.assertRaises(ValueError, lambda : api.mangal(usr = 'test'))
+    def test_if_pwd_then_usr(self):
+        self.assertRaises(ValueError, lambda : api.mangal(pwd = 'test'))
+    def test_usr_is_a_string(self):
+        self.assertRaises(TypeError, lambda : api.mangal(usr = 4, pwd = 'test'))
+    def test_pwd_is_a_string(self):
+        self.assertRaises(TypeError, lambda : api.mangal(usr = 'test', pwd = 4))
+    def test_minimal_elements_in_resources(self):
+        mg = api.mangal()
+        assert 'taxa' in mg.resources
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
