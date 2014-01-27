cc14560a195aa1dd5da98e2cfb5bfc554589fe6c
Tim Poisot <timothee.poisot@uqar.ca>
2014-01-26 19:35:16 -0500

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name='pymangal',
+        version='0.1.2',
+        description='A wrapper for the mangal API',
+        author='Timothée Poisot',
+        author_email='t.poisot@gmail.com',
+        url='http://pymangal.readthedocs.org/en/latest/',
+        packages=['pymangal']
+        )
