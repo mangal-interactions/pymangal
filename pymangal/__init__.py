b2cbe9dcaba832bf860977ece8f335f42d92e354
Tim Poisot <t.poisot@gmail.com>
2014-02-20 18:26:48 -0500

    INIT

--- pymangal/__init__.py
+++ pymangal/__init__.py
@@ -8,7 +8,7 @@ from api import mangal
 from makeschema import makeschema
 
 numVer = map(int, __version__.split('.'))
-if numVer[0] == '0':
+if numVer[0] == 0 :
     unstableMess = "pymangal v."+__version__+" - this is an UNSTABLE release"
     if not numVer[2] == 0 :
         unstableMess += ", development version"
